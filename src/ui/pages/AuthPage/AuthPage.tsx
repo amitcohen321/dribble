07f7b7141a297fbe16e8b1a7ca2370302e96be48
Amit Cohen <amit_cohen321@hotmail.com>
2024-11-26 19:30:24 +0200

    done with dashboard page

--- src/ui/pages/AuthPage/AuthPage.tsx
+++ src/ui/pages/AuthPage/AuthPage.tsx
@@ -1,21 +1,17 @@
 import { Box, Title, Text } from "@mantine/core";
 import styles from "./AuthPage.module.css"; // Import CSS module
-import dribblerImage from "../../../assets/auth-page/dribbler1.jpeg";
 
 const AuthPage = () => {
   return (
     <Box className={styles.pageContainer}>
-      <Box
-        className={styles.leftSection}
-        style={{ backgroundImage: `url(${dribblerImage})` }}
-      />
-      <Box className={styles.rightSection}>
+      <Box className={styles.imageSection} />
+      <Box className={styles.authSection}>
         <Box className={styles.contentBox}>
           <Box className={styles.logoPlaceholder} />
           <Title order={2} className={styles.slogan}>
             דריבל
           </Title>
-          <Text size="lg" style={{ marginTop: 10 }}>
+          <Text size="lg" mt={10}>
             ככה בונים חומה
           </Text>
           <Box className={styles.fbButton}>Login with Facebook</Box>
--- src/ui/pages/DashboardPage/DashboardPage.tsx
+++ src/ui/pages/DashboardPage/DashboardPage.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import {
+  Avatar,
+  Group,
+  Card,
+  Container,
+  Title,
+  Text,
+  ActionIcon,
+} from "@mantine/core";
+import { IconBallFootball } from "@tabler/icons-react";
+import styles from "./DashboardPage.module.css";
 
-const DashboardPage: React.FC = () => {
-  return <h1>DashboardPage</h1>;
-};
+const Dashboard = () =>
+  // {
+  //   // user,
+  //   //  games
+  // }
+  {
+    const user = {
+      firstName: "עמית",
+      profileImage:
+        "https://images.unsplash.com/photo-1612834787644-8b2b4e1f2c6b",
+    };
+    const games = [
+      {
+        title: "משחק 1",
+        description: "מגרש 1",
+      },
+      {
+        title: "משחק 2",
+        description: "מגרש 2",
+      },
+      {
+        title: "משחק 3",
+        description: "מגרש 3",
+      },
+    ];
 
-export default DashboardPage;
+    return (
+      <div className={styles.container}>
+        <header className={styles.topBar}>
+          <div className={styles.headerContent}>
+            <Group>
+              <Avatar src={user.profileImage} radius="xl" />
+              <Text className={styles.helloMessage}>{user.firstName}</Text>
+            </Group>
+            <img src="logo.png" alt="Logo" className={styles.logo} />
+          </div>
+        </header>
+
+        <Container
+          fluid
+          className={styles.dashboard}
+          style={{ margin: "10px" }}
+        >
+          {games.map((game, index) => (
+            <Card key={index} shadow="sm" className={styles.gameBox}>
+              <Title order={4}>{game.title}</Title>
+              <Text size="sm" color="dimmed">
+                {game.description}
+              </Text>
+            </Card>
+          ))}
+          <ActionIcon
+            radius="xl"
+            size={60}
+            className={styles.addGameButton}
+            style={{
+              position: "fixed",
+              left: "30px",
+              bottom: "90px",
+            }}
+            // onClick={onAddGame}
+            variant="filled"
+            color="violet"
+          >
+            <IconBallFootball size={30} />
+          </ActionIcon>
+        </Container>
+
+        <footer className={styles.footer}>
+          Built with <span className={styles.heart}>❤️</span> to make your life
+          easier
+        </footer>
+      </div>
+    );
+  };
+
+export default Dashboard;
